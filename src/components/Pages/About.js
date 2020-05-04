2c14d7f7a6a77329b5641cdaa1d4aec17342c49a
KzQ-96 <k.humienny1996@wp.pl>
2020-05-04 19:05:03 +0200

    ProjectEnd

---
+++ src/components/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>This is the TodoList ap v1.0.0 It is part of a my learn</p>
+        </>
+    )
+}
+
+export default About
